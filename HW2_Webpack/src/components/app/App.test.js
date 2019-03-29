755487dcd98e7aad88d5536b241b00201fd8737b
Tetiana_Chulkova <Tetiana_Chulkova@epam.com>
2019-03-29 16:45:27 +0200

    HW4_test: test for component app

--- HW2_Webpack/src/components/app/App.test.js
+++ HW2_Webpack/src/components/app/App.test.js
@@ -1,5 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react';
+
+
+configure({ adapter: new Adapter() });
+
 describe('Addition', () => {
     it('knows that 2 and 5 make 7', () => {
         expect(2 + 5).toBe(7);
+    });
+});
+
+describe('<App /> shallow rendering', () => {
+    it('should render App', () => {
+        const wrapper = shallow(<App />);
+    });
+    it('<h1>React Todo App!!!</h1>', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('h1').text()).toBe('React Todo App!!!');
+    });
+    it('matches the snapshot', () => {
+        const tree = shallow(<App />);
+        expect(toJSON(tree)).toMatchSnapshot();
     });
 });
