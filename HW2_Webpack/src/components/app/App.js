d7a459d5644ca94898d76fee4b41f53653e50efd
Tetiana_Chulkova <Tetiana_Chulkova@epam.com>
2019-05-21 14:47:05 +0300

    HW4_testing: removed excess block

--- HW2_Webpack/src/components/app/App.js
+++ HW2_Webpack/src/components/app/App.js
@@ -10,31 +10,13 @@ import List from "../list";
 
 export default class App extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            data: { dumbData }
-        }
-    }
-
-    setFilteredData = filter => {
-        const key = filter.toLowerCase();
-        const filteredList = this.props.data.data.filter((item) => {
-
-            return ( key === 'title') ?
-                (item.title.toLowerCase().search(this.state.searchTxt.toLowerCase()) !== -1 ) :
-                (item.genres.some((elem) => elem.toLowerCase().search(this.state.searchTxt.toLowerCase()) !== -1));
-        });
-        return this.setState ({ data: { filteredList }})
-    };
-
 
     render() {
     return (
         <div className="app">
             <ErrorBoundary>
                 <h1>React Todo App!!!</h1>
-                <Header data={dumbData} filteredItems={this.props.setFilteredData}/>
+                <Header data={dumbData} />
                 <List data={dumbData}/>
                 <Footer/>
             </ErrorBoundary>
